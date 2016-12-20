c6e2c815d40ff074a6294014d8e01ed6709482f7
vibol <vibolteav@gmail.com>
2016-12-20 17:17:25 +0700

    chore(): seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 # frozen_string_literal: true
-FactoryGirl.create(:user)
+FactoryGirl.create(:user, email: 'darren@rotati.com', password: 'z2uxb5', password_confirmation: 'z2uxb5')      if User.find_by(email: 'darren@rotati.com').nil?
+FactoryGirl.create(:user, email: 'vibol@rotati.com', password: 'z2uxb6', password_confirmation: 'z2uxb6')       if User.find_by(email: 'vibol@rotati.com').nil?
+FactoryGirl.create(:user, email: 'ainglorchhan@gmail.com', password: 'z2uxb7', password_confirmation: 'z2uxb7') if User.find_by(email: 'ainglorchhan@gmail.com').nil?
