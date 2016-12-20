82db4c8072fc0845ef4fbd527262ed4078b48f22
vibol <vibolteav@gmail.com>
2016-12-20 11:23:59 +0700

    fix(config): prod deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :stage, :production
 set :branch, 'stable'
+set :repo_url, "git@github.com:rotati/#{fetch(:application)}.git"
 
 server 'qbocambodia.com', user: 'deployer', roles: %w{app web db}
