736b6269642689f0fd3616317eee8619f04bf416
vibol <vibolteav@gmail.com>
2016-12-20 17:26:42 +0700

    chore(): add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 FactoryGirl.create(:user, email: 'darren@rotati.com', password: 'z2uxb5', password_confirmation: 'z2uxb5')      if User.find_by(email: 'darren@rotati.com').nil?
 FactoryGirl.create(:user, email: 'vibol@rotati.com', password: 'z2uxb6', password_confirmation: 'z2uxb6')       if User.find_by(email: 'vibol@rotati.com').nil?
+FactoryGirl.create(:user, email: 'yoan@rotati.com', password: 'z2uxb8', password_confirmation: 'z2uxb8')       if User.find_by(email: 'yoan@rotati.com').nil?
 FactoryGirl.create(:user, email: 'ainglorchhan@gmail.com', password: 'z2uxb7', password_confirmation: 'z2uxb7') if User.find_by(email: 'ainglorchhan@gmail.com').nil?
