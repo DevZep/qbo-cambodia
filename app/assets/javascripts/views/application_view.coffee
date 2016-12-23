c800985ef2ef136c9a258c26040abeb7867d02cf
vibol <vibolteav@gmail.com>
2016-12-23 09:16:58 +0700

    add flash

--- app/assets/javascripts/views/application_view.coffee
+++ app/assets/javascripts/views/application_view.coffee
@@ -1,3 +1,9 @@
 class Views.ApplicationView
   render: ->
+    _autoCloseAlert()
   cleanup: ->
+
+
+  _autoCloseAlert= ->
+    $('.alert').fadeTo(3000, 500).slideUp 500, ->
+      $('.alert').slideUp 500
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,4 +37,23 @@ module ApplicationHelper
       end
     end
   end
+
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when 'success'
+        'alert-success'
+      when 'error'
+        'alert-danger'
+      when 'alert'
+        'alert-danger'
+      when 'notice'
+        'alert-info'
+      else
+        flash_type.to_s
+    end
+  end
+
+  def toggle_class(condition, class_name)
+    class_name if condition
+  end
 end
