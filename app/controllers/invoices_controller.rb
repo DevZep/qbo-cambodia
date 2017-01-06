1df5b9846df372c18032e780e4fed15d68f322d7
raizer123 <caili.cl29@gmail.com>
2017-01-06 10:40:48 +0700

    udpate features

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class InvoicesController < ApplicationController
 
+
   include Kaminari
 
   layout false, only: :show
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
   get '/companies/:company_id/invoices/debits', to: 'invoices#debit', as: :invoices_debits_path
   get '/companies/:company_id/invoices/invoices', to: 'invoices#invoice', as: :invoices_invoices_path
 
+
+  # get '/companies/:company_id/invoices/:id.pdf' => redirect 
+
+
   resources :companies, only: [] do
     resources :invoices, only: [:show, :index]
   end
