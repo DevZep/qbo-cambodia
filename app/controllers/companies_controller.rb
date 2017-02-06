69605c5b562741d8f3618a68b4449a18738119a4
raizer123 <caili.cl29@gmail.com>
2017-02-06 17:16:01 +0700

    test

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -25,7 +25,7 @@ class CompaniesController < ApplicationController
           get_all_invoice = invoice_service.get_all_invoices
         rescue Quickbooks::AuthorizationFailure => e
           if e == Quickbooks::AuthorizationFailure
-            redirect_to root_path, notice: "Fail"
+            get_all_invoices = []
           end
         end
         #show both next available id
