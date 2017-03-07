fb9ec76a8b273e78651d22ef5ffe5591e78699de
raizer123 <caili.cl29@gmail.com>
2017-03-07 12:08:51 +0700

    fix word break

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,16 @@
-# config valid only for current version of Capistrano
 lock '3.7.0'
 
 set :application, "qbo-cambodia"
-set :repo_url, "git@github.com-qbo:rotati/#{fetch(:application)}.git"
+set :repo_url, "git@github.com:rotati/#{fetch(:application)}.git"
 
-# Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/#{fetch(:application)}"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :scm, :git
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
 set :pty, true
+set :keep_releases, 5
 
 set :rvm_ruby_version, '2.3.3@qbo-cambodia'
 set :rvm_roles, [:app, :web]
@@ -27,18 +19,6 @@ set :passenger_restart_with_touch, true
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets')
 set :linked_files, fetch(:linked_files, []).push('.env')
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-set :keep_releases, 3
 
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,4 +2,5 @@ require 'dotenv'
 Dotenv.load
 
 set :stage, :staging
+set :repo_url, "git@github.com-qbo:rotati/#{fetch(:application)}.git"
 server 'qbo-cambodia.rotati.com', user: 'deployer', roles: %w{app web db}
