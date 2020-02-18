8e010e80a9fbe7cff13ca785e6872758f4930e83
vibol.t <vibolteav@gmail.com>
2020-02-19 00:16:14 +0700

    refresh token if needed

--- app/models/qbo_credential.rb
+++ app/models/qbo_credential.rb
@@ -28,4 +28,24 @@ class QboCredential < ApplicationRecord
             :oauth2_refresh_token_expires_at, presence: true
 
   validates :user, presence: true
+
+  def refresh_tokens
+    return if oauth2_access_token_expires_at? && oauth2_access_token_expires_at > Time.current
+
+    oauth_client = IntuitOAuth::Client.new(
+      ENV['QBO_CLIENT_ID'],
+      ENV['QBO_CLIENT_SECRET'],
+      '',
+      Rails.env.development? ? 'sandbox' : 'production'
+    )
+
+    response = oauth_client.token.refresh_tokens(oauth2_refresh_token)
+
+    self.update(
+      oauth2_access_token: response.access_token,
+      oauth2_refresh_token: response.refresh_token,
+      oauth2_access_token_expires_at: response.expires_in.seconds.from_now,
+      oauth2_refresh_token_expires_at: response.x_refresh_token_expires_in.seconds.from_now
+    )
+  end
 end
--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -2,8 +2,10 @@ class BaseService
   attr_reader :service, :qbo_credential
 
   def initialize(qbo_credential)
-    access_token    = OAuth2::AccessToken.new(QB_OAUTH2, qbo_credential.oauth2_access_token, refresh_token: qbo_credential.oauth2_refresh_token)
     @qbo_credential = qbo_credential
+    @qbo_credential.refresh_tokens
+
+    access_token    = OAuth2::AccessToken.new(QB_OAUTH2, qbo_credential.oauth2_access_token, refresh_token: qbo_credential.oauth2_refresh_token)
     @service        = service_class.new(access_token: access_token, realm_id: qbo_credential.company_id)
   end
 
